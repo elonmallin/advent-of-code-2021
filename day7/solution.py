6c6d65046a0ed2f16447efa3b25399a96bf92728
Elon Mallin <elon.mallin@gmail.com>
2021-12-07 19:49:41 +0100

    feat(day7): add day7 part 2

--- day7/solution.py
+++ day7/solution.py
@@ -8,6 +8,7 @@ input_file = open(os.path.join(os.path.dirname(__file__), './input.txt'))
 data = list(map(lambda l: int(l), input_file.readlines()[0].split(',')))
 
 
+# %%
 def calc_fuel_cost(i: int, data: list[int]):
     cost = 0
     for n in data:
@@ -28,3 +29,27 @@ def solve_part_1(data: list[int]) -> int:
 
 
 print(solve_part_1(data))
+
+
+# %%
+def calc_fuel_cost_exp(i: int, data: list[int]):
+    cost = 0
+    for n in data:
+        diff = abs(n - i)
+        cost += int((1 + diff) / 2 * diff)
+
+    return cost
+
+
+def solve_part_2(data: list[int]) -> int:
+    cheapest_cost = 99999999
+
+    for i in range(len(data)):
+        cost = calc_fuel_cost_exp(i, data)
+        if cost < cheapest_cost:
+            cheapest_cost = cost
+
+    return cheapest_cost
+
+
+print(solve_part_2(data))
